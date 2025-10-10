f0af2c3173ec3251cd8183eff885dbb059761c0d
victorlinares100 <linares28072004@gmail.com>
2025-10-10 15:33:36 -0300

    WindowSizeUtils.kt arreglado

--- app/src/main/java/com/example/proyecto/ui/utils/WindowSizeUtils.kt
+++ app/src/main/java/com/example/proyecto/ui/utils/WindowSizeUtils.kt
@@ -1,6 +1,18 @@
 package com.example.proyecto.ui.utils
 
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 
-import androidx.activity.compose.LocalActivity
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun obtenerWindowSizeClass(): WindowSizeClass {
+    val context = LocalContext.current
+    val activity = context as Activity
+    return calculateWindowSizeClass(activity)
+}
 
 
